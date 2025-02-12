2350140b7b56a5c3d494d9f2d6ec34cfc959ebb0
Kaylie1a <kchanthalima76@gmail.com>
2025-02-11 20:18:47 -0800

    script.js

--- script.js
+++ script.js
@@ -59,11 +59,11 @@ const messages = [
     "Are you positive?",
     "Pookie please...",
     "Just think about it!",
-    "If you say no, I will be really sad...",
-    "I will be very sad...",
-    "I will be very very very sad...",
+    "If you say no, I'll be big sad...",
+    "I will be mopey...",
+    "I will be super sad...",
     "Ok fine, I will stop asking...",
-    "Just kidding, say yes please! ❤️"
+    "Just kidding, say yes please, you'll be rewarded greatly! ❤️"
 ];
 
 let messageIndex = 0;
